65e693dec1050ea76f4235a0363e2db4a18401ea
Wolfgang Fahl <wf@bitplan.com>
2020-11-17 11:02:29 +0100

    initial check in

---
+++ ceurws/workshop.py
@@ -0,0 +1,25 @@
+'''
+Created on 2020-11-12
+
+@author: wf
+'''
+from urllib.request import urlopen
+import xmltodict
+
+class Workshop(object):
+    '''
+
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+
+    @staticmethod
+    def ofURI(uri):
+        xml=urlopen(uri).read().decode()
+        ws=Workshop()
+        ws.wsdict=xmltodict.parse(xml)
+        return ws
+        

2a91abcaf2e4ef96e6b6c45461101606f9f017fa
tholzheim <tim.holzheim@rwth-aachen.de>
2021-12-14 12:13:37 +0100

    Started new volume structure

---
+++ ceurws/ceurws.py
@@ -0,0 +1,319 @@
+import datetime
+import os
+
+from lodstorage.entity import EntityManager
+from lodstorage.jsonable import JSONAble
+from lodstorage.storageconfig import StorageConfig
+
+
+CEURWS_URL="http://ceur-ws.org"
+CACHE_FILE=os.path.dirname(__file__) + "/resources/ceurws.db"
+CONFIG=StorageConfig(cacheFile=CACHE_FILE)
+
+
+class Volume(JSONAble):
+    """
+    Represents a volume in ceur-ws
+    """
+
+    def getSamples(self):
+        samples=[
+            {
+                "number": 2436,
+                "url": "http://ceur-ws.org/Vol-2436/",
+                "title": "Evaluation and Experimental Design in Data Mining and Machine Learning",
+                "fullTitle": "1st Workshop on Evaluation and Experimental Design in Data Mining and Machine Learning",
+                "acronym": "EDML 2019",
+                "lang": "en",
+                "location": "Calgary, Alberta, Canada",
+                "country": "Canada",
+                "region":"Alberta",
+                "city": "Calgary",
+                "ordinal": 1,
+                "date":datetime.datetime(year=2019, month=5, day=4),
+                "dateFrom":"",
+                "dateTo":"",
+                "pubYear":2019,
+                "pubDate":"2019-09-08",
+                "submitDate":"2019-07-28",
+                "conference": Conference,
+                "editors":[Editor],
+                "sessions":[Session]
+            }
+        ]
+
+    @property
+    def year(self):
+        if hasattr(self, "date") and isinstance(self.date, datetime.datetime):
+            return self.date.year
+
+    @property
+    def volNumber(self)->str:
+        if hasattr(self, "number") and self.number:
+            return f"Vol-{self.number}"
+        return None
+
+    @property
+    def sessions(self):
+        """
+        sessions of this volume
+        """
+        return self._sessions
+
+    @sessions.setter
+    def sessions(self, session):
+        # ToDo: Adjust to proper 1:n handling
+        if hasattr(self, '_sessions') and isinstance(self._sessions, list):
+            self._sessions.append(session)
+        else:
+            setattr(self, '_sessions', session)
+
+    @property
+    def papers(self):
+        """
+        papers of this volume
+        """
+        return
+
+    def getSubmittingEditor(self):
+        """
+        Retruns the Editor that sumbitted the volume
+        """
+        submitter=None
+        if hasattr(self, "editors"):
+            for editor in self.editors:
+                if isinstance(editor, Editor):
+                    if getattr(editor, "submitted", False):
+                        submitter=editor
+                        break
+        return submitter
+
+
+class VolumeManager(EntityManager):
+    """
+    Contains multiple ceurws volumes
+    """
+
+    def __init__(self, tableName:str="volumes"):
+        super(VolumeManager, self).__init__(listName="volumes",
+                                            clazz=Volume,
+                                            tableName=tableName,
+                                            entityName=Volume.__class__.__name__,
+                                            primaryKey="number",
+                                            entityPluralName="volumes",
+                                            config=CONFIG,
+                                            name=self.__class__.__name__)
+
+
+    def loadFromBackup(self):
+        self.fromStore(cacheFile=CACHE_FILE)
+
+
+
+
+class Paper(JSONAble):
+    """
+    Represents a conference
+    """
+
+    @staticmethod
+    def getSamples():
+        samples=[
+            {
+                "id": "Vol-2436/s1/summary",  # id is constructed with volume and position → <volNumber>/s<position>/<type>_<position_relative_to_type>
+                "type":"summary",
+                "position": 0,
+                "title": "1st Workshop on Evaluation and Experimental Design in Data Mining and Machine Learning (EDML 2019)",
+                "pdf": "http://ceur-ws.org/Vol-2436/summary.pdf",
+                "pagesFrom": 1,
+                "pagesTo": 3,
+                "authors": ["Eirini Ntoutsi", "Erich Schubert", "Arthur Zimek", "Albrecht Zimmermann"]
+            },
+            {
+                "id": "Vol-2436/s1/invited_1",
+                "type":"invited",
+                "position": 1,
+                "title": "Evaluation of Unsupervised Learning Results: Making the Seemingly Impossible Possible",
+                "pdf": "http://ceur-ws.org/Vol-2436/invited_1.pdf",
+                "pagesFrom": 4,
+                "pagesTo": 4,
+                "authors": ["Ricardo J. G. B. Campello"]
+            },
+            {
+                "id": "Vol-2436/s1/article_1",
+                "type": "article",
+                "position": 2,
+                "title": "EvalNE: A Framework for Evaluating Network Embeddings on Link Prediction",
+                "pdf": "http://ceur-ws.org/Vol-2436/article_2.pdf",
+                "pagesFrom": 5,
+                "pagesTo": 13,
+                "authors": ["Alexandru Mara", "Jefrey Lijffijt", "Tijl De Bie"]
+            }
+        ]
+
+
+class PaperManager(EntityManager):
+    """
+    Contains multiple ceurws sessions
+    """
+
+    def __init__(self):
+        super(PaperManager, self).__init__(listName="papers",
+                                            clazz=Editor,
+                                            tableName="papers",
+                                            entityName=Session.__class__.__name__,
+                                            primaryKey="id",
+                                            entityPluralName="papers",
+                                             config=CONFIG,
+                                            name=self.__class__.__name__)
+
+
+class Session(JSONAble):
+    """
+    Represents a session in ceur-ws
+    """
+
+    def getSamples(self):
+        samples=[
+            {
+                "id":"Vol-2436/s1",  # id is constructed with volume and position → <volNumber>/s<position>
+                "volume":{"Vol-2436": Volume},  # n:1 relation / reporting chain
+                "title":"Information Technologies and Intelligent Decision Making Systems II",
+                "position": 1,
+                "papers": {   # 1:n relation / command chain
+                    "VOL-2436/s1/p1":Paper,
+                    "VOL-2436/s1/p2":Paper
+                }
+            }
+        ]
+
+    @property
+    def volume(self)->Volume:
+        if self._volume is None and self._volumeKey is not None:
+                # load volume
+                lVolume="ToDo:"
+                # set volume
+                self._volume=lVolume
+                pass
+        else:
+            return self._volume
+
+
+    @property
+    def papers(self, cached:bool=False): #dict: str→Paper
+        if cached:
+            # check if cached
+            pass
+        else:
+            #load papers
+            if cached:
+                #set papers
+                pass
+        return self._papers
+
+    @papers.setter
+    def papers(self, paper:Paper):
+        # ToDo: Adjust to proper 1:n handling
+        if hasattr(self, '_papers') and isinstance(self._papers, dict):
+            self._papers.update(paper.id, paper)
+        else:
+            setattr(self,'_papers',paper)
+
+
+
+class SessionManager(EntityManager):
+    """
+    Contains multiple ceurws sessions
+    """
+
+    def __init__(self):
+        super(SessionManager, self).__init__(listName="sessions",
+                                            clazz=Session,
+                                            tableName="sessions",
+                                            entityName=Session.__class__.__name__,
+                                            primaryKey="id",  #ToDo: check if just the title is a sufficent key or if an ID must be added
+                                            entityPluralName="sessions",
+                                             config=CONFIG,
+                                            name=self.__class__.__name__)
+
+
+
+class Editor(JSONAble):
+    """
+    Represents a volume editor
+    """
+
+    @staticmethod
+    def getSamples():
+        samples=[
+            {
+                "id": "Vol-2436/John Doe",
+                "name": "John Doe",
+                "homepage": "http://www.example.org/john",
+                "country": "Germany",
+                "affiliation": "Leibniz University Hannover & L3S Research Center",
+                "submitted": False
+            },
+            {
+                "id": "Vol-2436/Jane Doe",
+                "name": "Jane Doe",
+                "homepage": "http://www.example.org/jane",
+                "country": "Germany",
+                "affiliation": "Technical University Dortmund",
+                "submitted": True
+            }
+        ]
+
+
+class EditorManager(EntityManager):
+    """
+    Contains multiple ceurws sessions
+    """
+
+    def __init__(self):
+        super(EditorManager, self).__init__(listName="editors",
+                                            clazz=Editor,
+                                            tableName="editors",
+                                            entityName=Session.__class__.__name__,
+                                            primaryKey="id",
+                                            entityPluralName="editors",
+                                             config=CONFIG,
+                                            name=self.__class__.__name__)
+
+
+class Conference(JSONAble):
+    """
+    Represents a conference
+    """
+
+    @staticmethod
+    def getSamples():
+        samples=[
+            {
+                "id": "Vol-2436",
+                "fullTitle": "SIAM International Conference on Data Mining",
+                "homepage": "https://www.siam.org/Conferences/CM/Main/sdm19",
+                "acronym": "SDM 2019"
+            }
+        ]
+
+
+class ConferenceManager(EntityManager):
+    """
+    Contains multiple ceurws sessions
+    """
+
+    def __init__(self):
+        super(ConferenceManager, self).__init__(listName="conferences",
+                                            clazz=Editor,
+                                            tableName="conferences",
+                                            entityName=Session.__class__.__name__,
+                                            primaryKey="id",
+                                            entityPluralName="conferences",
+                                             config=CONFIG,
+                                            name=self.__class__.__name__)
+
+if __name__ == '__main__':
+    manager=VolumeManager()
+    manager.loadFromBackup()
+    print(manager.getList())
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+from collections import OrderedDict
+
+setup(name='pyCEURmake',
+      version='0.0.1',
+      description='python api providing access to view and add ceur-ws volumes',
+      long_description_content_type='text/markdown',
+      url='https://github.com/WolfgangFahl/pyCEURmake',
+      download_url='https://github.com/WolfgangFahl/pyCEURmake',
+      author='wf',
+      license='Apache',
+      project_urls=OrderedDict(
+        (
+            ("Code", "https://github.com/WolfgangFahl/pyCEURmake"),
+            ("Issue tracker", "https://github.com/WolfgangFahl/pyCEURmake/issues"),
+        )
+      ),
+      classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9'
+      ],
+      packages=['ceurws'],
+      package_data={'ceurws': ['resources/templates/*.jinja','resources/templates/*.html']},
+      install_requires=[
+          'pylodstorage>=0.0.69',
+          'python-dateutil',
+          'ConferenceCorpus>=0.0.26',
+          'pyFlaskBootstrap4>=0.2.13',
+          'wikirender>=0.0.33',
+          'flask-dropzone',
+          'odfpy'
+      ],
+      entry_points={
+         'console_scripts': [
+             'pyCEURmake = ceurws.webserver:main',
+      ],
+    },
+      zip_safe=False)

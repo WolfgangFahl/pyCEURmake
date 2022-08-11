2b53b0ad4bdab019e7fe40966c1c6e36dfcf9d40
Wolfgang Fahl <wf@bitplan.com>
2022-08-11 15:13:45 +0200

    adds version and tries fixing requirements

---
+++ ceurws/version.py
@@ -0,0 +1,15 @@
+'''
+Created on 2022-09-11
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for pyCEURmake
+    '''
+    name="pyCEURmake"
+    version='0.0.2'
+    date = '2020-11-12'
+    updated = '2022-08-11'
+    description='python api providing access to view and add ceur-ws volumes'

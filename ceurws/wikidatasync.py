d0a86a390f284cd631537425cbefef6056102aba
Wolfgang Fahl <wf@bitplan.com>
2022-08-15 19:55:42 +0200

    fixes type

--- ceurws/wikidatasync.py
+++ ceurws/wikidatasync.py
@@ -13,52 +13,65 @@ from lodstorage.lod import LOD
 from ceurws.ceur_ws import VolumeManager, CEURWS
 from lodstorage.sparql import SPARQL
 from lodstorage.sql import SQLDB
-from lodstorage.query import QueryManager,EndpointManager
+from lodstorage.query import QueryManager, EndpointManager
+
 
 class WikidataSync(object):
     '''
     synchronize with wikidata
     '''
 
-    def __init__(self):
+    def __init__(self, debug:bool=False):
         '''
         Constructor
+        
+        Args:
+            debug(bool): if True switch on debugging
         '''
+        self.debug = debug
         self.prepareVolumeManager()
         self.prepareRDF()
-        self.wdQuery=self.qm.queriesByName["Proceedings"]
+        self.wdQuery = self.qm.queriesByName["Proceedings"]
+        self.wd = Wikidata(baseurl="https://www.wikidata.org", debug=debug)
+        
+    def login(self):
+        # @FIXME add username/password handling (see gsimport)
+        self.wd.loginWithCredentials()
+        
+    def logout(self):
+        self.wd.logout()
         
     def prepareRDF(self):
         # SPARQL setup
-        self.endpoints=EndpointManager.getEndpoints(lang="sparql")
-        self.endpointConf=self.endpoints.get("wikidata")
-        self.sparql=SPARQL(self.endpointConf.endpoint)
-        path=os.path.dirname(__file__)
-        qYamlFile=f"{path}/resources/queries/ceurws.yaml"
+        self.endpoints = EndpointManager.getEndpoints(lang="sparql")
+        self.endpointConf = self.endpoints.get("wikidata")
+        self.sparql = SPARQL(self.endpointConf.endpoint)
+        path = os.path.dirname(__file__)
+        qYamlFile = f"{path}/resources/queries/ceurws.yaml"
         if os.path.isfile(qYamlFile):
-            self.qm=QueryManager(lang="sparql",queriesPath=qYamlFile)
+            self.qm = QueryManager(lang="sparql", queriesPath=qYamlFile)
         
     def prepareVolumeManager(self):
         '''
         prepare my volume manager
         '''
-        self.vm=VolumeManager()
+        self.vm = VolumeManager()
         if Download.needsDownload(CEURWS.CACHE_FILE):
             self.vm.loadFromIndexHtml(force=True)
             self.vm.store()
         else:
             self.vm.loadFromBackup()
         self.volumesByNumber, _duplicates = LOD.getLookup(self.vm.getList(), 'number')
-        self.volumeList=self.vm.getList()
-        self.volumeCount=len(self.volumeList)
+        self.volumeList = self.vm.getList()
+        self.volumeCount = len(self.volumeList)
         
     def update(self):
-        wdRecords=self.sparql.queryAsListOfDicts(self.wdQuery.query)
-        #wdRecordsByVolume=
-        sqldb=SQLDB(CEURWS.CACHE_FILE)
-        primaryKey="URN_NBN"
-        withCreate=True
-        withDrop=True
+        wdRecords = self.sparql.queryAsListOfDicts(self.wdQuery.query)
+        # wdRecordsByVolume=
+        sqldb = SQLDB(CEURWS.CACHE_FILE)
+        primaryKey = "URN_NBN"
+        withCreate = True
+        withDrop = True
         sqldb.createTable(wdRecords, "Proceedings", primaryKey, withCreate, withDrop)
         return wdRecords
 
@@ -90,89 +103,102 @@ class WikidataSync(object):
         wdItems = [record.get("event") for record in qres]
         return wdItems
 
-    def addProceedingToWikidata(self, record:dict):
+    def addProceedingToWikidata(self, record:dict, write:bool=True, ignoreErrors:bool=False):
         """
         Creates a wikidata entry for the given record
+        
+        Args:
+            record(dict): the data to add
+            write(bool): if True actually write
+            ignoreErrors(bool): if True ignore errors
+            
         """
+        # see https://docs.google.com/spreadsheets/d/1fTMUuXXq_7lJgUzwLntrg4GUQ-lmlXYhS-I1Fhwc9dQ/edit#gid=0
         wdMetadata = [
-            {"Entity": "proceedings",
-             "Column": None,
-             "PropertyName": "instanceof",
-             "PropertyId": "P31",
-             "Value": "Q1143604",
-             "Type": None,
-             "Qualifier": None,
-             "Lookup": None
-             },
-            {"Entity": "proceedings",
-             "Column": "title",
-             "PropertyName": "title",
-             "PropertyId": "P1476",
-             "Type": "text",
-             "Qualifier": None,
-             "Lookup": ""
-             },
-            {"Entity": "proceedings",
-             "Column": "short name",
-             "PropertyName": "short name",
-             "PropertyId": "P1813",
-             "Type": "text",
-             "Qualifier": None,
-             "Lookup": ""
-             },
-            {"Entity": "proceedings",
-             "Column": None,
-             "PropertyName": "part of the series",
-             "PropertyId": "P179",
-             "Value": "Q27230297",
-             "Type": None,
-             "Qualifier": None,
-             "Lookup": None
-             },
-            {"Entity": "proceedings",
-             "Column": "volume",
-             "PropertyName": "volume",
-             "PropertyId": "P478",
-             "Type": None,
-             "Qualifier": "part of the series",
-             "Lookup": ""
-             },
-            {"Entity": "proceedings",
-             "Column": "pubDate",
-             "PropertyName": "publication date",
-             "PropertyId": "P577",
-             "Type": "date",
-             "Qualifier": None,
-             "Lookup": ""
-             },
-            {"Entity": "proceedings",
-             "Column": "ceurwsUrl",
-             "PropertyName": "described at URL",
-             "PropertyId": "P973",
-             "Type": "url",
-             "Qualifier": None,
-             "Lookup": ""
-             },
-            {"Entity": "proceedings",
-             "Column": "fullWorkUrl",
-             "PropertyName": "full work available at URL",
-             "PropertyId": "P953",
-             "Type": "url",
-             "Qualifier": None,
-             "Lookup": ""
-             },
-            {"Entity": "proceedings",
-             "Column": "urn",
-             "PropertyName": "URN-NBN",
-             "PropertyId": "P4109",
-             "Type": "extid",
-             "Qualifier": None,
-             "Lookup": ""
+            {
+                "Entity": "proceedings",
+                "Column": None,
+                "PropertyName": "instanceof",
+                "PropertyId": "P31",
+                "Value": "Q1143604",
+                "Type": None,
+                "Qualifier": None,
+                "Lookup": None
+            },
+            {
+                "Entity": "proceedings",
+                "Column": None,
+                "PropertyName": "part of the series",
+                "PropertyId": "P179",
+                "Value": "Q27230297",
+                "Type": None,
+                "Qualifier": None,
+                "Lookup": None
+            },
+            {
+                "Entity": "proceedings",
+                "Column": "volume",
+                "PropertyName": "volume",
+                "PropertyId": "P478",
+                "Type": "string",
+                "Qualifier": "part of the series",
+                "Lookup": ""
+            },
+            {
+                "Entity": "proceedings",
+                "Column": "short name",
+                "PropertyName": "short name",
+                "PropertyId": "P1813",
+                "Type": "text",
+                "Qualifier": None,
+                "Lookup": ""
+            },
+            {
+                "Entity": "proceedings",
+                "Column": "title",
+                "PropertyName": "title",
+                "PropertyId": "P1476",
+                "Type": "text",
+                "Qualifier": None,
+                "Lookup": ""
+            },
+            {
+                "Entity": "proceedings",
+                "Column": "pubDate",
+                "PropertyName": "publication date",
+                "PropertyId": "P577",
+                "Type": "date",
+                "Qualifier": None,
+                "Lookup": ""
+            },
+            {
+                "Entity": "proceedings",
+                "Column": "ceurwsUrl",
+                "PropertyName": "described at URL",
+                "PropertyId": "P973",
+                "Type": "url",
+                "Qualifier": None,
+                "Lookup": ""
+            },
+            {
+                "Entity": "proceedings",
+                "Column": "fullWorkUrl",
+                "PropertyName": "full work available at URL",
+                "PropertyId": "P953",
+                "Type": "url",
+                "Qualifier": None,
+                "Lookup": ""
+            },
+            {
+                "Entity": "proceedings",
+                "Column": "urn",
+                "PropertyName": "URN-NBN",
+                "PropertyId": "P4109",
+                "Type": "extid",
+                "Qualifier": None,
+                "Lookup": ""
              }
         ]
         mapDict, _ = LOD.getLookup(wdMetadata, "PropertyId")
-        wd = Wikidata(baseurl="https://www.wikidata.org")
-        wd.loginWithCredentials()
-        qId, errors = wd.addDict(row=record, mapDict=mapDict, write=True)
-        wd.logout()
+        qId, errors = self.wd.addDict(row=record, mapDict=mapDict, write=write, ignoreErrors=ignoreErrors)
         return qId, errors

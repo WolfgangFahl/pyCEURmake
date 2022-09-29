ef3114dca529be8e04188a14326df5aa205945de
tholzheim <tim.holzheim@rwth-aachen.de>
2022-09-29 16:16:37 +0200

    Added editor and affiliation parser

--- ceurws/volumeparser.py
+++ ceurws/volumeparser.py
@@ -3,6 +3,11 @@ Created on 2022-08-14
 
 @author: wf
 '''
+import re
+import typing
+
+from bs4 import BeautifulSoup, PageElement, NavigableString
+
 from utils.webscrape import WebScrape
 from ceurws.textparser import Textparser
 
@@ -26,7 +31,7 @@ class VolumeParser(Textparser):
         self.timeout=timeout
         self.scrape=WebScrape(timeout=timeout)
         
-    def volumeUrl(self,volnumber):
+    def volumeUrl(self, volnumber: typing.Union[str, int]):
         '''
         get the url for the given volume number
         
@@ -39,10 +44,26 @@ class VolumeParser(Textparser):
         # e.g. http://ceur-ws.org/Vol-2635/
         url=f"{self.baseurl}/Vol-{volnumber}"
         return url
+
+    def getSoup(self, url: str) -> BeautifulSoup:
+        """
+        get the beautiful Soup parser for the given url
+        Args:
+            url: url to parse
+
+        Returns:
+            parsed webpage
+        """
+        return self.scrape.getSoup(url, showHtml=self.showHtml)
         
-    def parse(self,url):
+    def parse(self, url: str) -> dict:
         '''
         parse the given url
+        Args:
+             url: URL to parse the volume information from
+
+         Returns:
+             extracted information
         '''
         # first try RDFa annotations
         _valid,_err,scrapedDict=self.parseRDFa(url)
@@ -50,7 +71,7 @@ class VolumeParser(Textparser):
             scrapedDict[key]=Textparser.sanitize(scrapedDict[key])
         
         # second part
-        soup=self.scrape.getSoup(url, showHtml=self.showHtml)
+        soup = self.getSoup(url)
         for descValue in ["description","descripton"]:
             # descripton is a typo in the Volume index files not here!
             firstDesc=soup.find("meta", {"name" : descValue})
@@ -90,6 +111,127 @@ class VolumeParser(Textparser):
             scrapedDict["acronym"] = scrapedDict["h1"]
             
         return scrapedDict
+
+    def parseEditors(self, soup: BeautifulSoup):
+        """
+        parse all editor information contained in the given soup
+        parse all information between <b> Edited by </b> ... <hr>
+        Args:
+            soup: volume web page
+        """
+        possible_start_elements = soup.find_all("b")
+        edited_by = [e for e in possible_start_elements if "edited by" in e.text.lower()][0]
+        editor_h3 = edited_by.find_next("h3")
+        editor_records: typing.Dict[str, dict] = dict()
+        editor_spans = editor_h3.find_all(attrs={"class":"CEURVOLEDITOR"})
+        if editor_spans is not None and len(editor_spans) > 0:
+
+            for editor_span in editor_spans:
+                editor_name = editor_span.text
+                editor = {"name": editor_name}
+                if editor_span.parent.name == "a":
+                    homepage = editor_span.parent.attrs.get("href", None)
+                    editor["homepage"] = homepage
+                    affiliation_keys = editor_span.parent.next_sibling.text
+                else:
+                   affiliation_keys = editor_span.next_sibling.text
+                editor["affiliation_keys"] = affiliation_keys.strip()
+                editor_records[editor_name] = editor
+        else:
+            editor_elements = []
+            group_elements = []
+            if editor_h3.next_sibling.next_sibling.name == "h3":
+                while editor_h3.next_sibling.next_sibling.name == "h3" and editor_h3.text.strip() != "":
+                    editor_elements.append(editor_h3.contents)
+                    editor_h3 = editor_h3.next_sibling.next_sibling
+            else:
+                for child in editor_h3.childGenerator():
+                    if child.name == "br":
+                        editor_elements.append(group_elements)
+                        group_elements = []
+                    else:
+                        group_elements.append(child)
+            for elements in editor_elements:
+                text = "".join([e.text for e in elements]).strip()
+                affiliation_key = text.split(" ")[-1]
+                editor_name = text[:-len(affiliation_key)]
+                links = [e for e in elements if e.name == "a"]
+                if len(links) > 0:
+                    homepage = links[0].attrs.get("href", None)
+                else:
+                    homepage = None
+                editor = {"name": editor_name, "homepage": homepage, "affiliation_key": affiliation_key}
+                editor_records[editor_name] = editor
+        affiliation_keys = {editor.get("affiliation_key")
+                            for editor in editor_records.values()
+                            if editor.get("affiliation_key", None) is not None}
+        affiliation_map = self.parseAffiliationMap(editor_h3.next_sibling)
+        for editor_record in editor_records.values():
+            keys = re.split('[, ]', editor_record.get("affiliation_keys", ""))
+            editor_affiliations = []
+            for key in keys:
+                if key in affiliation_map:
+                    editor_affiliations.append(affiliation_map.get(key.strip()))
+            editor_record["affiliation"] = editor_affiliations
+        return editor_records
+
+    def parseAffiliationMap(self, start: PageElement) -> dict:
+        """
+        Parse out the affiliations and their reference key
+        Args:
+            soup:
+
+        Returns:
+            dict
+        """
+        end = start.find_next("hr")
+        affiliations_elements = []
+        group_elements = []
+        for element in start.previous.nextGenerator():
+            if element.name in ["br", "hr"]:
+                affiliations_elements.append(group_elements)
+                group_elements = []
+            elif isinstance(element, NavigableString) and element.text.strip() == "":
+                pass
+            elif element.name == "h3":
+                # elements inside the element are included through the nextGenerator
+                pass
+            else:
+                group_elements.append(element)
+            if element == end:
+                break
+        affiliations_elements = [x for x in affiliations_elements if x != []]
+        affiliation_map = dict()
+        for elements in affiliations_elements:
+            if isinstance(elements[0], NavigableString) and " " in elements[0].text.strip():
+                text_containing_key = elements[0].text.strip()
+                key = text_containing_key.split(" ")[0]
+                key_element = NavigableString(value=key)
+                text_element = NavigableString(value=text_containing_key[len(key):])
+                elements = [key_element, text_element, *elements[1:]]
+            key = elements[0].text.strip()
+            text_elements = []
+            link_elements = []
+            for element in elements[1:]:
+                if isinstance(element, NavigableString):
+                    text_elements.append(element)
+                elif element.name == "a":
+                    link_elements.append(element)
+            affiliation = "".join([elem.text for elem in text_elements])
+            affiliation = affiliation.replace("\n", "").replace("\t", "").replace("\r", "")
+            if affiliation.startswith(key):
+                affiliation = affiliation[len(key):]
+            homepages = []
+            for element in link_elements:
+                if hasattr(element, "attrs") and element.attrs.get("href", None) is not None:
+                    homepage = element.attrs.get("href", None)
+                    homepages.append(homepage)
+            if key is not None and key != "":
+                key = key.strip(".")
+                affiliation_map[key] = {"name": affiliation, "homepage": homepages}
+        return affiliation_map
+
+
 
     def parseRDFa(self,url):
         scrapeDescr=[
--- tests/test_volumeparser.py
+++ tests/test_volumeparser.py
@@ -3,6 +3,10 @@ Created on 2022-08-14
 
 @author: wf
 '''
+import pprint
+import time
+import unittest
+
 from tests.basetest import Basetest
 from ceurws.volumeparser import VolumeParser
 from ceurws.ceur_ws import VolumeManager
@@ -64,4 +68,80 @@ class TestVolumeParser(Basetest):
         self.assertEqual("SWAT4LS 2008", scrapedDict.get("acronym"))
         self.assertEqual("http://www.swat4ls.org/", scrapedDict.get("homepage"))
         print(scrapedDict)
-            
+
+    def test_parseEditors(self):
+        """
+        tests parseEditors
+        """
+        test_params = [  # (volumeNumber, #editors, #editorHomepages, #affiliations)
+            (3200, 8, 3, 6), (3100, 2, 0, 1), (3000, 2, 1, 2), (2800, 2, 0, 2), (2500, 2, 2, 0), (2000, 5, 4, 4),
+            (1500, 5, 5, 5), (1000, 9, 1, 6), (700, 3, 1, 2), (600, 1, 0, 1),
+            # (500, 3, 3, 2),
+            # (550, 2, 2, 2),
+            # (250, 7, 7, 5),
+            # (200, 3, 3, 1), (100, 4, 4, 3), (50, 2, 1, 2), (10, 3, 3, 3), (1, 4, 0, 2),
+            (3212, 2, 2, 2),  # multiple affiliations for one editor
+            # (3211, 5, 5, 5),  # affiliation missing → error on volume page
+            (3207, 5, 4, 7),  # sup with link to affiliation
+            (3203, 2, 2, 3),  # multiple affiliations for one editor with reference seperator
+            (3199, 4, 0, 4),  # sup without link to affiliation (reference and key different)
+            (3188, 4, 0, 3),
+            (3148, 2, 1, 3),
+            (3139, 3, 3, 3),
+            # (3127, 9, 0, 9),  # special case of editor definition
+            # (3116, 2, 2, 2),  # affiliation reference included in link
+        ]
+        for param in test_params[0:]:
+            with self.subTest(f"test editor parsing for Vol-{param[0]}", param=param):
+                number, exp_editors, exp_homepages, exp_affiliations = param
+                url = self.volumeParser.volumeUrl(number)
+                soup = self.volumeParser.getSoup(url)
+                res = self.volumeParser.parseEditors(soup)
+                #pprint.pprint(res)
+                number_of_editors = len(res)
+                number_of_hompages = len(
+                        [e.get("homepage")
+                         for e in res.values()
+                         if e.get("homepage", None) is not None])
+                affiliations = []
+                for e in res.values():
+                    affiliations.extend([a.get("name") for a in e.get("affiliation")])
+                number_of_affiliations = len(set(affiliations))
+                print(
+                    f"Vol-{number}:#editors={number_of_editors} #hompages={number_of_hompages} #affiliations={number_of_affiliations} ({url})")
+                self.assertEqual(exp_editors, number_of_editors)
+                self.assertEqual(exp_homepages, number_of_hompages)
+                self.assertEqual(exp_affiliations, number_of_affiliations)
+
+    @unittest.skipIf(True, "Only for manual testing")
+    def test_parseEditors_stressTest(self):
+        """
+        ToDo: How many can I extract correctly?
+        """
+        total = 3225
+        end = 600
+        with open("/tmp/log_ceurws_editor_parsing.txt", mode="a") as fp:
+            for i in range(total, end, -1):
+                if i%100 == 0:
+                    time.sleep(10)
+                url = self.volumeParser.volumeUrl(i)
+                soup = self.volumeParser.getSoup(url)
+                msg = f"({i:04}/{total})"
+                try:
+                    res = self.volumeParser.parseEditors(soup)
+                except Exception as ex:
+                    msg += " ❌ Error"
+                    res = None
+                    print(ex)
+                if res is not None:
+                    number_of_editors = len(res)
+                    number_of_hompages = len([e.get("homepage") for e in res.values() if e.get("homepage", None) is not None])
+                    affiliations = []
+                    for e in res.values():
+                        affiliations.extend([a.get("name") for a in e.get("affiliation")])
+                    number_of_affiliation = len(set(affiliations))
+                    number_of_editors_with_affiliations = len([True for e in res.values() if e.get("affiliation", None) is not None])
+                    something_fishy = "❌" if number_of_editors !=number_of_editors_with_affiliations else ""
+                    msg += f"{something_fishy} #editors={number_of_editors} #affiliations={number_of_affiliation} #hompages={number_of_hompages} ({url})"
+                print(msg)
+                fp.write(msg+"\n")

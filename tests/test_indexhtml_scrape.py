8ee555db46219935abfff35999039ea21fc5a5b1
tholzheim <tim.holzheim@rwth-aachen.de>
2023-05-19 11:14:37 +0200

    added test case and documentation #75 #76

--- tests/test_indexhtml_scrape.py
+++ tests/test_indexhtml_scrape.py
@@ -100,3 +100,25 @@ class TestIndexHtml(Basetest):
                 print(f"{volume.url}:{volume.acronym}:{volume.desc}:{volume.h1}:{volume.title}")
         if withStore:
             vm.store()
+
+    def test_setSeeAlsoVolumes(self):
+        """
+        tests extraction of series link over seeAlso
+        """
+        vm = VolumeManager()
+        htmlText = vm.getIndexHtml(force=False)
+        indexParser = IndexHtmlParser(htmlText, debug=self.debug)
+        volumes = indexParser.parse()
+        expected_see_also = {  # volume: expected see also list
+            3333: [3067],
+            3321: [3000, 2709],
+            3308: [2668, 2123, 1624, 1466, 1252, 1062, 972, 959, 672, 433, 331, 162, 110],
+            2298: [2035, 1909],
+            1609: [1391, 1180, 1179, 1178, 1177, 1176, 1175, 1174, 1173, 1172, 1171, 1170, 1169, 1168, 1167, 1166],
+            246: [116, 80, 56, 36, 27, 20, 12, 6]
+        }
+        for volume_number, see_also in expected_see_also.items():
+            with self.subTest(volume_number=volume_number, see_also=see_also):
+                actual_see_also = volumes.get(volume_number).get("seealso", None)
+                see_also = [f"Vol-{vn}" for vn in see_also]
+                self.assertListEqual(see_also, actual_see_also)

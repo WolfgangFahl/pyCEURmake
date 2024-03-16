2463539e01ed3a4b42d4c0578ef813d5e1df2aaf
Wolfgang Fahl <wf@bitplan.com>
2024-03-16 19:19:22 +0100

    improves dblp2 model

--- ceurws/models/dblp2.py
+++ ceurws/models/dblp2.py
@@ -3,19 +3,8 @@ Created on 2024-03-16
 
 @author: wf
 """
- 
 from sqlmodel import SQLModel,Field
 from typing import Optional
-
-class Paper(SQLModel, table=True):
-    """
-    A paper indexed in DBLP with additional details. The paper URL is used as the unique identifier.
-    """
-    paper: str = Field(primary_key=True)
-    proceeding: str = Field(index=True)
-    volume_number: str = Field(index=True)
-    title: str
-    pdf_url: Optional[str]
     
 class Scholar(SQLModel, table=True):
     """
@@ -27,6 +16,16 @@ class Scholar(SQLModel, table=True):
     orcid_id: Optional[str] = None
     gnd_id: Optional[str] = None
     
+class Paper(SQLModel, table=True):
+    """
+    A paper indexed in DBLP with additional details. The paper URL is used as the unique identifier.
+    """
+    paper: str = Field(primary_key=True)
+    proceeding: str = Field(index=True)
+    volume_number: str = Field(index=True)
+    title: str
+    pdf_url: Optional[str]
+    
 class Proceeding(SQLModel, table=True):
     """
     A proceeding indexed in DBLP with additional details.
@@ -35,4 +34,17 @@ class Proceeding(SQLModel, table=True):
     volume_number: int = Field(primary_key=True)
     title: str
     dblp_event_id: Optional[str] = None
+    
+class Editorship(SQLModel, table=True):
+    """
+    Represents the relationship between a scholar and a proceeding, indicating the scholar's role as an editor.
+    """
+    volume_number: int = Field(foreign_key="proceeding.volume_number", primary_key=True)
+    dblp_author_id: str = Field(foreign_key="scholar.dblp_author_id", primary_key=True)
 
+class Authorship(SQLModel, table=True):
+    """
+    Represents the relationship between a scholar and a paper, capturing the authorship details.
+    """
+    paper: str = Field(foreign_key="paper.paper", primary_key=True)
+    dblp_author_id: str = Field(foreign_key="scholar.dblp_author_id", primary_key=True)

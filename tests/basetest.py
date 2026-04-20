06c2685b69ce13dcc10282505f082727c44479ed
Tim Holzheim <holzheim@dbis.rwth-aachen.de>
2026-04-20 18:30:38 +0200

    removes entity_fishing and opentapioca

--- tests/basetest.py
+++ tests/basetest.py
@@ -15,9 +15,6 @@ import requests
 from lodstorage.query import Endpoint
 from lodstorage.sparql import SPARQL
 
-from ceurws.services.entity_fishing import ENTITY_FISHING_ENDPOINT
-from ceurws.services.opentapioca import OPENTAPIOCA_ENDPOINT
-
 
 class Basetest(TestCase):
     """
@@ -109,37 +106,3 @@ def requires_sparql_endpoint(*, endpoint: Endpoint):
     except Exception as e:
         print(e)
     return pytest.mark.skipif(is_unavailable, reason=f" SPARQL endpoint {endpoint.name} is unavailable")
-
-
-def _requires_entity_fishing_endpoint():
-    """
-    test case requires entity fishing endpoint
-    """
-    is_unavailable = True
-    try:
-        url = f"{ENTITY_FISHING_ENDPOINT}/service/kb/concept/Q5"
-        resp = requests.get(url)
-        resp.raise_for_status()
-        is_unavailable = False
-    except Exception as e:
-        print(e)
-    return pytest.mark.skipif(is_unavailable, reason="entity fishing endpoint is not available")
-
-
-def _requires_opentapioca_endpoint():
-    """
-    test case requires entity fishing endpoint
-    """
-    is_unavailable = True
-    try:
-        url = OPENTAPIOCA_ENDPOINT
-        resp = requests.get(url)
-        resp.raise_for_status()
-        is_unavailable = False
-    except Exception as e:
-        print(e)
-    return pytest.mark.skipif(is_unavailable, reason="opentapioca is not available")
-
-
-requires_entity_fishing_endpoint = _requires_entity_fishing_endpoint()
-requires_opentapioca_endpoint = _requires_opentapioca_endpoint()
